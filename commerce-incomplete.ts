28c22f2b612d10619f9ceccb16443c748efb4a5e
Mr. Fleming <efleming@stpatrick.org>
2018-11-05 13:13:05 -0600

    Add files via upload

---
+++ commerce-incomplete.ts
@@ -0,0 +1,28 @@
+class Bank{
+
+
+}//end of Bank class
+
+
+
+
+class Transaction{
+
+
+}//end of Transaction class
+
+
+
+
+class ATM{
+
+
+}//end of ATM class
+
+
+
+
+class DebitCard{
+
+
+}//end of Debit Card class
