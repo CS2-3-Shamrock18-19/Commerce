042a983435b9b5df9f35aa4330f1088455ea4fa2
Mr. Fleming <efleming@stpatrick.org>
2018-11-06 13:03:17 -0600

    started adding a main method

--- commerce-incomplete.ts
+++ commerce-incomplete.ts
@@ -21,6 +21,66 @@ class Transaction{
 
 
 
+
+
+
+//creating the tests
+function main () : void{
+
+//Create some customers
+let charliedata = [,,,];
+const Charlie = new Customer(...charliedata);
+
+let dilliondata = [,,,];
+const Dillion = new Customer(...dilliondata);
+
+let delanodata = [,,,];
+const Delano = new Customer(...delanodata);
+
+let jamesdata = [,,,];
+const James = new Customer(...jamesdata);
+
+let kewendata = [,,,];
+const Kewen = new Customer(...kewendata);
+
+
+
+let customerdata = [charliedata,dilliondata,delanodata,jamesdata,kewendata];
+
+
+
+//Make a Bank
+const BankofAmerica = new Bank(customerdata);
+
+
+//Have some customers make transactions with the bank
+
+
+
+
+
+}
+
+//executing the tests
+main();
+
+
+
+
+
+
+
+
+
+
+
+/**
+***
+*** THIS IS FOR THE NEXT PHASE...PHASE 2!
+***
+**/
+
+
 class ATM{
 
 
